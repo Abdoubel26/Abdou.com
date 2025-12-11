06366c0491193c2c4f95737d0ab07479470c328e
Abdoubel26 <xenom442025@outlook.com>
2025-12-11 18:35:38 +0100

    Converted to TypeScript + Updates

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+
+import pic from './assets/propic.png'
+
+function App() {
+  
+  return (
+    <>
+     
+    <header className='header'>
+        <h1>Abdou Belounis <span className='rocket'>🚀</span></h1>
+        <h2>Coder,  Independent learner</h2>
+    </header>
+
+    <section className="profile">
+      <img src={pic} alt="Profile photo" id="profile-pic" title="Abdou Belounis" />
+        <p>Space Exploration Advocate, Indie Hacker</p>
+    </section>
+<hr></hr>
+    
+    <section className="about">
+       <h3>About Me</h3>
+
+<p>
+    Let's connect and explore the future together! 
+    <a href="mailto:xenom442025@outlook.com?subject=XXX&body=Hello, Abdou Belounis, I’d love to..."  style={{color: "#ff5733", textDecoration: "none", fontWeight: "bold"}}>
+<span className="span">Reach out here</span></a>
+</p>
+    Follow me for more updates:  
+    <a href="https://x.com/abdou_belounis" style={{color: "#c2dcc2", textDecoration: "none", fontWeight: "bold"}}> <span className="span">X (Twitter)</span> </a>|  
+    <a href="https://instagram.com/abdou_belounis26" style={{color: "#c2dcc2", textDecoration: "none", fontWeight: "bold"}}> <span className='span' >Instagram</span> </a> |
+    <a href="https://github.com/Abdoubel26" style={{color: "#c2dcc2", textDecoration: "none", fontWeight: "bold"}}> <span className='span'> Github </span></a>
+</section>
+
+
+<footer>
+  <p>© 2025 Abdou Belounis. Made with ☕ & curiosity.</p>
+</footer>
+
+
+    </>
+  )
+}
+
+export default App
