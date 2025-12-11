6c0769de6ef8eaacec56eb5e62b4b37eb093c522
Abdoubel26 <xenom442025@outlook.com>
2025-12-11 21:18:01 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_headers',
+          dest: '.' // This puts _headers directly into dist/
+        }
+      ]
+    })
+  ]
 })
