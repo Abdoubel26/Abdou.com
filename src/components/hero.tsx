04a799ebff3d3af1f7288b4517b3c7731fec60e9
Abdoubel26 <xenom442025@outlook.com>
2025-12-17 22:00:16 +0100

    Added Hero + about me + started Projects section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,7 @@
 function Hero() {
     return ( 
     <div>
-        <div className=" bg-[url('/bg.jpg')] bg-cover bg-no-repeat bg-fixed bg-center">
+        <div className=" bg-[url('/bg.jpg')] bg-cover bg-no-repeat bg-fixed bg-center backdrop-blur-2xl">
             <div className="flex justify-center items-center h-70 flex-col">
                 <h1 className="text-5xl text-white font-semibold ">Hello, I'm Abdou</h1>
                 <p className=" text-cyan-200 text-lg font-semibold bg-black/40 rounded-lg p-1">I create interesting web applications!</p>
