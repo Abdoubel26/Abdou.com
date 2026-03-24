03356a6eab4a6bfffca34d9197c431321547fb8d
Abdoubel26 <abdoubelounis09@gmail.com>
2026-03-24 14:28:21 +0100

    remove viteStaticCopy from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
@@ -9,14 +8,6 @@ export default defineConfig({
     port: 5173
   },
   plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_headers',
-          dest: '.' // This puts _headers directly into dist/
-        }
-      ]
-    }), tailwindcss()
+    react(), tailwindcss()
   ]
 })
